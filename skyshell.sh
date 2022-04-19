1104abcc390975358f2cbc7a566ca6f666ef7730
Skylighty <pawel.gasiewski@student.put.poznan.pl>
2022-04-19 22:49:29 +0200

    fixed tee idents

--- skyshell.sh
+++ skyshell.sh
@@ -176,27 +176,21 @@ do
             FILE=$homedir/.ssh/id_rsa
             if [[ -f "$FILE" ]]; then
                 tee -a $homedir/.zshrc > /dev/null << END
-                env=~/.ssh/agent.env
-    
-                agent_load_env () { test -f "$env" && . "$env" >| /dev/null ; }
-    
-                agent_start () {
-                    (umask 077; ssh-agent >| "$env")
-                    . "$env" >| /dev/null ; }
-    
-                agent_load_env
-    
-                # agent_run_state: 0=agent running w/ key; 1=agent w/o key; 2=agent not running
-                agent_run_state=$(ssh-add -l >| /dev/null 2>&1; echo $?)
-    
-                if [ ! "$SSH_AUTH_SOCK" ] || [ $agent_run_state = 2 ]; then
-                    agent_start
-                    ssh-add ~/.ssh/id_rsa
-                elif [ "$SSH_AUTH_SOCK" ] && [ $agent_run_state = 1 ]; then
-                    ssh-add ~/.ssh/id_rsa
-                fi
-    
-                unset env
+env=~/.ssh/agent.e
+agent_load_env () { test -f "$env" && . "$env" >| /dev/null ;
+agent_start () {
+    (umask 077; ssh-agent >| "$env")
+    . "$env" >| /dev/null ;
+agent_load_e
+# agent_run_state: 0=agent running w/ key; 1=agent w/o key; 2=agent not running
+agent_run_state=$(ssh-add -l >| /dev/null 2>&1; echo $
+if [ ! "$SSH_AUTH_SOCK" ] || [ $agent_run_state = 2 ]; then
+    agent_start
+    ssh-add ~/.ssh/id_rsa
+elif [ "$SSH_AUTH_SOCK" ] && [ $agent_run_state = 1 ]; then
+    ssh-add ~/.ssh/id_rsa
+fi
+unset env
 END
             else
                 echo -e "\n${RED}ERROR${NC} - no ssh-key available for user, use ${YELL}ssh-keygen${NC} and try again!\n"
