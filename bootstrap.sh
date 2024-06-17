4aacb9b79210515f53eac99d9a76d4b24405acfc
pawel.gasiewski <pawel.gasiewski@hawetelekom.com>
2024-06-17 23:53:00 +0200

    fixed paths... again

--- bootstrap.sh
+++ bootstrap.sh
@@ -132,16 +132,16 @@ case $choice in
         wget -q https://nodejs.org/dist/v20.11.1/node-v20.11.1-linux-x64.tar.gz
         mkdir $HOME/static
         tar -xvzf node-v20.11.1-linux-x64.tar.gz -C $HOME/static/ > /dev/null 2>&1
-        echo "export PATH=${PATH}:/${HOME}/static/node-v20.11.1-linux-x64/bin" >> $HOME/.zshrc
-        export PATH=${PATH}:/${HOME}/static/node-v20.11.1-linux-x64/bin
+        echo "export PATH=$PATH:${HOME}/static/node-v20.11.1-linux-x64/bin" >> $HOME/.zshrc
+        export PATH=${PATH}:${HOME}/static/node-v20.11.1-linux-x64/bin
         sudo rm node-v20.11.1-linux-x64.tar.gz
 
         # Install neovim
         wget -q https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz 
         tar -xvzf nvim-linux64.tar.gz > /dev/null 2>&1
         mv ./nvim-linux64 $HOME/static/
-        echo "export PATH=${PATH}:/${HOME}/static/nvim-linux64/bin" >> $HOME/.zshrc
-        export PATH=${PATH}:/${HOME}/static/nvim-linux64/bin
+        echo "export PATH=$PATH:${HOME}/static/nvim-linux64/bin" >> $HOME/.zshrc
+        export PATH=${PATH}:${HOME}/static/nvim-linux64/bin
         sudo rm -rf ./nvim-linux64.tar.gz 
 
         # Resolve npm global problem
@@ -149,10 +149,10 @@ case $choice in
         npm config set prefix '~/.npm-global'
         echo 'export NPM_CONFIG_PREFIX=~/.npm-global' >> "$HOME/.bashrc" 
         echo 'export NPM_CONFIG_PREFIX=~/.npm-global' >> "$HOME/.zshrc"
-        echo 'export PATH="${PATH}:/$HOME/.npm-global/bin"' >> "$HOME/.profile"
-        echo 'export PATH="${PATH}:/$HOME/.npm-global/bin"' >> "$HOME/.zshrc"
+        echo 'export PATH="${PATH}:$HOME/.npm-global/bin"' >> "$HOME/.profile"
+        echo 'export PATH="${PATH}:$HOME/.npm-global/bin"' >> "$HOME/.zshrc"
         export NPM_CONFIG_PREFIX=$HOME/.npm-global
-        export PATH=${PATH}:/$HOME/.npm-global/bin
+        export PATH=${PATH}:$HOME/.npm-global/bin
         echo 'source $HOME/.profile' >> "$HOME/.bashrc"
         source "$HOME/.profile"
 
